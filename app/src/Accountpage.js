49677722684b12fb38dddfeaaf5caa110f0bc962
Dan W <daniel.winn@student.awesomeinc.org>
2022-11-29 18:52:16 +0000

    set up the frontend basic

---
+++ app/src/Accountpage.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import logo from './Images/Danstagram_logo.png'
+
+export default function Account() {
+    return (
+        <div className='container-fluid justify-content-center'>
+            <div className='row'>
+                <div className='col-9'>
+                    <h1>Danstagram</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import Account from './Accountpage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Account />
+    </div>
+  );
+}
+
+export default App;
